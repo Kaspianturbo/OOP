ff474e973bdcbc3e230ed1d4259832ac4a4f81dc
Віктор Шевченко <kaspianturbo@gmail.com>
2020-10-09 10:29:43 +0300

    Lab6 is complete

---
+++ Lab6_Regex/Analizator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Lab6_Regex
+{
+    static public class Analizator
+    {
+        static Regex letter;
+        static Regex figure;
+        static Regex identify;
+        static Regex figereEnum;
+        static Regex mark;
+        static Regex regex;
+        static public bool IsValid(string str)
+        {
+            letter = new Regex("[a-z]");
+            figure = new Regex("[0-9]");
+            identify = new Regex($"{letter}" + "(" + $"{letter}|{figure}" + ")*");
+            figereEnum = new Regex("[0-9]{1,4}");
+            mark = new Regex($"{identify}|{figereEnum}");
+            regex = new Regex($"^LABEL ((({identify})|({figereEnum}))(, (({identify})|({figereEnum})))*)$");
+            Console.WriteLine(letter);
+            Console.WriteLine(figure);
+            Console.WriteLine(identify);
+            Console.WriteLine(figereEnum);
+            Console.WriteLine(mark);
+            Console.WriteLine(regex);
+            return regex.IsMatch(str);
+        }
+    }
+}
---
+++ Lab6_Regex/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Lab6_Regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string str = Console.ReadLine();
+            bool isValid = Analizator.IsValid(str);
+            Console.WriteLine(isValid);
+            Console.ReadKey();
+        }
+    }
+}
